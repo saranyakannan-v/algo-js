84823b8b7a72717e4b131d0a41db30095b41cc30
Saranya Kannan <saranyakannan.v@gmail.com>
2020-10-12 11:13:30 +0200

    intro

---
+++ JavaScript 01 - Introduction/1.0.js
@@ -0,0 +1,24 @@
+const readlineSync = require("readline-sync");
+
+let a = 10;
+let b = 2;
+
+let addition = a + b;
+
+console.log(addition);
+
+let substraction = a - b;
+
+console.log(substraction);
+
+let multiplication = a * b;
+
+console.log(multiplication);
+
+let division = a / b;
+
+console.log(division);
+
+let modulo = a % b;
+
+console.log(modulo);
