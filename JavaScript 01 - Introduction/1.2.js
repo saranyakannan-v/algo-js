781b4a0b7dcbff7f30f605ae75a4fc93685be8b9
Saranya Kannan <saranyakannan.v@gmail.com>
2020-10-12 12:13:02 +0200

    1.2

---
+++ JavaScript 01 - Introduction/1.2.js
@@ -0,0 +1,6 @@
+/*Define three variables: name, firstName and city. Display them like this: "Your name is Gerard Lambert and you live in Paris.".*/
+const readlineSync = require("readline-sync");
+let name = "Lambert";
+let firstName = "Gerard";
+let city = "Paris";
+console.log("Your name is ", firstName, name, "and you live in", city + ".");
