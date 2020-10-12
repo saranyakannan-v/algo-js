deebd126a3ac6f10f719b78022f33c81fa7057b4
Saranya Kannan <saranyakannan.v@gmail.com>
2020-10-12 11:31:47 +0200

    intro

--- JavaScript 01 - Introduction/1.0.js
+++ JavaScript 01 - Introduction/1.0.js
@@ -1,10 +1,11 @@
+/*This line of code at the top of your file to import the little library we installed earlier*/
 const readlineSync = require("readline-sync");
-
+/*variable declaration using let- it's a block scope*/
 let a = 10;
 let b = 2;
-
+/*using arithmetic operators*/
 let addition = a + b;
-
+/*console.log() is often used to debug your code or to display small message*/
 console.log(addition);
 
 let substraction = a - b;
