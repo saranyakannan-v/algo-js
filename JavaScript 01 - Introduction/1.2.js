5a091465654e04bbb704d51aeebb9285fffe82aa
Saranya Kannan <saranyakannan.v@gmail.com>
2020-10-12 12:16:10 +0200

    1.2

--- JavaScript 01 - Introduction/1.2.js
+++ JavaScript 01 - Introduction/1.2.js
@@ -3,4 +3,5 @@ const readlineSync = require("readline-sync");
 let name = "Lambert";
 let firstName = "Gerard";
 let city = "Paris";
+/*", comma is used to give a space between text" and "+ is used for without space between the text "*/
 console.log("Your name is ", firstName, name, "and you live in", city + ".");
