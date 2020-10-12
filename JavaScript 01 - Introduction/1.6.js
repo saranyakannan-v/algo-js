98062e5e9f97b1d46b42939039550e29451623fd
Saranya Kannan <saranyakannan.v@gmail.com>
2020-10-12 14:28:26 +0200

    1.6

--- JavaScript 01 - Introduction/1.6.js
+++ JavaScript 01 - Introduction/1.6.js
@@ -1,5 +1,6 @@
 /*Ask two integers to the user. Display the rest of the integer division of the two numbers.*/
 const readlineSync = require("readline-sync");
+/*this questionInt will allow the user only to type integer not the characters */
 let int1 = readlineSync.questionInt("Enter the first integer :");
 let int2 = readlineSync.questionInt("Enter the second integer :");
 let output = int1 / int2;
