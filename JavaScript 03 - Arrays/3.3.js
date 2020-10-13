8dfc31650ca445fab4478450f746d9f9c7cdaef3
Saranya Kannan <saranyakannan.v@gmail.com>
2020-10-13 12:18:55 +0200

    3.3

---
+++ JavaScript 03 - Arrays/3.3.js
@@ -0,0 +1,27 @@
+// Write a program that will create a duplicate of a given array.
+
+// Bonus: make a first version that will only do it using push(). Make a second version that uses a single method call to perform the copy.
+
+let arr = [1, 2, 3, 4, 5];
+let arrDup = []
+
+for (let i = 0; i < arr.length; i++) {
+    //push method - Adds one or more elements to the end of an array and returns the new length of the array.
+    arrDup.push(arr[i]);
+}
+
+console.log(arrDup);
+
+
+/* second method:
+let arrCopy=[];
+for (let i = 0; i< arr.length; i++){
+    arrCopy=arr[i];
+}
+console.log(arrCopy);*/
+
+/*Third method
+let arr = [1, 2, 3, 4, 5];
+//slice method - Adds and/or removes elements from an array.
+let arrCopy = arr.slice();
+console.log(arrCopy);*/
