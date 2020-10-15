4a269c75127ab40425add7f0296f08ae16b542dd
Saranya Kannan <saranyakannan.v@gmail.com>
2020-10-15 15:07:51 +0200

    4.6

--- JavaScript 04 - Functions/4.6.js
+++ JavaScript 04 - Functions/4.6.js
@@ -1,19 +1,19 @@
 // Create a function factorial(a) that returns the factorial of a number.
 // That function must be recursive.
 
-const readlineSync = require("readline-sync");
-
 // factorial - The product of an integer and all the integers below it.
 // eg: 5! = 5 * 4 * 3 * 2 * 1
 // n! = n * (n - 1) * (n - 2) * ...*1
+// Recursion is a technique for iterating over an operation by having a function call itself repeatedly until it arrives at a result.(if else)
 
+const readlineSync = require("readline-sync");
+let a = new Number(readlineSync.questionInt("Please enter a number : "));
 
 function factorial(a) {
-    let total = 1;
-    while (a > 0) {
-        total *= a;
-        a = a - 1;
+    if (a == 1) {
+        return a;
+    } else {
+        return a * (factorial(a - 1));
     }
-    return total;
 }
-console.log("The factorial of a number is : " + factorial(5));
+console.log("The factorial of a number is : " + factorial(a));
